96e4a5f9b893ef29e15ed15d191673eccd1f9b66
Elliot Davies <elliot.a.davies@gmail.com>
2023-12-03 22:48:06 +0000

    Day 02 part 1

--- src/AOC2023/Day02.hs
+++ src/AOC2023/Day02.hs
@@ -1,17 +1,77 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
+
 module AOC2023.Day02
   ( Input,
     Output,
     part1,
     part2,
+    cubes,
+    handful,
+    game,
+    Color (..),
+    Game (..),
   )
 where
+
+import Control.Applicative ((<|>))
+import Debug.Trace (traceShow)
+import Text.Parsec (many1, parse, sepBy)
+import Text.Parsec.Char (digit, space, string)
+import Text.Parsec.String (Parser)
 
 type Input = String
 
 type Output = Int
 
+data Game = Game {gameId :: Int, red :: Int, green :: Int, blue :: Int}
+  deriving (Eq, Show)
+
+game :: Parser Game
+game = do
+  string "Game "
+  gameId <- read <$> many1 digit
+  string ": "
+  handfuls <- handful `sepBy` string "; "
+
+  let maxReds = maxSeen Red handfuls
+  let maxBlues = maxSeen Blue handfuls
+  let maxGreens = maxSeen Green handfuls
+
+  pure $ Game gameId maxReds maxGreens maxBlues
+  where
+    maxSeen :: Color -> [[(Color, Int)]] -> Int
+    maxSeen color = maximum . map snd . filter ((== color) . fst) . concat
+
+handful :: Parser [(Color, Int)]
+handful = cubes `sepBy` string ", "
+
+data Color = Red | Green | Blue
+  deriving (Eq, Show)
+
+readColor :: String -> Color
+readColor = \case
+  "red" -> Red
+  "blue" -> Blue
+  "green" -> Green
+  other -> error $ "Unexpected color: " ++ other
+
+cubes :: Parser (Color, Int)
+cubes = do
+  num <- read <$> many1 digit
+  space
+  (,num) . readColor <$> (string "red" <|> string "green" <|> string "blue")
+
+filterGames :: [Game] -> [Game]
+filterGames = filter (\(Game {red, green, blue}) -> red <= 12 && green <= 13 && blue <= 14)
+
 part1 :: Input -> Output
-part1 _ = 0
+part1 input =
+  let results = sequence $ map (parse game "") (filter (not . null) $ lines input)
+   in case results of
+        Right gs -> sum $ map gameId $ filterGames gs
+        Left err -> length $ traceShow err (show err)
 
 part2 :: Input -> Output
 part2 _ = 0
--- test/AOC2023/Day02Spec.hs
+++ test/AOC2023/Day02Spec.hs
@@ -2,17 +2,28 @@ module AOC2023.Day02Spec where
 
 import AOC2023.Day02
 import Test.Hspec
+import Text.Parsec (parse)
 
 spec :: Spec
 spec =
   describe "Day02" $ do
+    it "parsers" $ do
+      parse cubes "" "3 blue" `shouldBe` Right (Blue, 3)
+      parse cubes "" "12 red" `shouldBe` Right (Red, 12)
+      parse cubes "" "1 green" `shouldBe` Right (Green, 1)
+
+      parse handful "" "1 blue, 2 green" `shouldBe` Right [(Blue, 1), (Green, 2)]
+      parse handful "" "1 green, 2 red;" `shouldBe` Right [(Green, 1), (Red, 2)]
+
+      parse game "" "Game 23: 1 green, 2 red; 3 blue, 14 green" `shouldBe` Right (Game 23 2 14 3)
+
     it "Part 1 test" $ do
       input <- readFile "inputs/day02/part1_test.txt"
       part1 input `shouldBe` 8
 
     it "Part 1 real" $ do
-      -- input <- readFile "inputs/day02/real.txt"
-      pending
+      input <- readFile "inputs/day02/real.txt"
+      part1 input `shouldBe` 2505
 
     it "Part 2 test" $ do
       -- input <- readFile "inputs/day02/part2_test.txt"
